0ff717ee69820ec4b3772306dd8d1db477ad625f
Jet Fontanilla <pauljetfontanilla@gmail.com>
2022-02-01 20:32:54 +0900

    add directory checking

--- trainer.py
+++ trainer.py
@@ -1,10 +1,15 @@
 import plyvel
 import os
+from pathlib import Path
 
 roaming = os.getenv('APPDATA')
 vs_db_folder = rf'{roaming}\Vampire_Survivors\Local Storage\leveldb'
+vs_db_path = Path(vs_db_folder)
 
-db = plyvel.DB(vs_db_folder, create_if_missing=False)
-db.put(b'_file://\x00\x01CapacitorStorage.Coins', b'\x0199999')
+if (vs_db_path.exists() and vs_db_path.is_dir()):
+    db = plyvel.DB(vs_db_folder, create_if_missing=False)
+    db.put(b'_file://\x00\x01CapacitorStorage.Coins', b'\x0199999')
 
-print('Coins applied')
+    print('Coins applied')
+else:
+    print('Game directory not found. exiting')
